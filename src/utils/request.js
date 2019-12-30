2113c0879a219ad05c3408d1a4438746fbe5743b
Zeeco Dai <zeeco.dai@think-bridge.com>
2019-12-30 13:21:04 +0800

    chore: request process

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,11 @@
 import axios from "axios";
-
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "http://47.101.131.65:3011"
+    : "http://localhost:3011";
 const service = axios.create({
   timeout: 100000,
-  baseURL: "http://localhost:3011"
+  baseURL
 });
 
 service.interceptors.response.use(response => response.data);
