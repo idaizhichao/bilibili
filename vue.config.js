0c5657a052b2e1930167f713ee67e53bbecc1ca1
Zeeco Dai <zeeco.dai@think-bridge.com>
2019-12-30 13:22:12 +0800

    chore: build add .gz file

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
+// 生成.gz文件
+const CompressionPlugin = require("compression-webpack-plugin");
+
 module.exports = {
-  lintOnSave: false
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin({
+        test: /\.js$|\.html$|\.css/,
+        threshold: 10240,
+        deleteOriginalAssets: false
+      })
+    ]
+  },
+  lintOnSave: false,
+  productionSourceMap: false
 };
