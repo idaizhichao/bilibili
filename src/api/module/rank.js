9fdd96a6f172624518d30b8e9d785240b2e991db
Zeeco Dai <zeeco.dai@think-bridge.com>
2020-01-08 17:04:51 +0800

    chore: add rank related interface

--- src/api/module/rank.js
+++ src/api/module/rank.js
@@ -1,6 +1,8 @@
 import { axios } from "@/utils/request";
 const api = {
-  getRegion: "ranking/region" //分类排行
+  getRegion: "ranking/region", //分类排行
+  getRankingById: "ranking", //根据rid 查询排行榜
+  getRankingPartitions: "ranking/partitions" //根据rid 查询排行榜
 };
 
 export function getRegion(rId, day = 3) {
@@ -14,6 +16,19 @@ export function getRegion(rId, day = 3) {
     params: data
   });
 }
+
+export function getRankingById(rId, day = 3) {
+  return axios({
+    url: `${api.getRankingById}/${rId}/${day}`
+  });
+}
+export function getRankingPartitions() {
+  return axios({
+    url: api.getRankingPartitions
+  });
+}
 export default {
-  getRegion
+  getRegion,
+  getRankingById,
+  getRankingPartitions
 };
