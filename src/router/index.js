87105f61aceab5a5e2016dfb3e2ae10699ab645e
Zeeco Dai <zeeco.dai@think-bridge.com>
2020-01-08 17:09:31 +0800

    add: ranking page

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,18 @@ export default new Router({
           name: "player",
           component: () =>
             import(/* webpackChunkName: "player" */ "../views/player")
+        },
+        {
+          path: "/video",
+          name: "Video",
+          component: () =>
+            import(/* webpackChunkName: "player" */ "../views/player/Video")
+        },
+        {
+          path: "/ranking/:tid",
+          name: "Ranking",
+          component: () =>
+            import(/* webpackChunkName: "ranking" */ "../views/ranking")
         }
       ]
     }
